e14b4109259f0a3aa0d2b5f123878bd63541f434
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2025-04-21 13:29:22 +0300

    `use()` must await the promise at the end of the block even if the user called `catch()`

--- private/use.ts
+++ private/use.ts
@@ -4,6 +4,9 @@
 	Not awaiting a rejected promise can lead to unhandled promise rejections, which can cause the Deno process to terminate.
 	In the other hand, it's quite easy to miss the awaiting, when an exception is thrown in the middle of the block.
 
+	Rejected and not explicitly awaited promise will throw exception at the end of the block.
+	If the promise was awaited in the block, the exception will be thrown only at the point of awaiting, and will be ignored at the end of the block.
+
 	Example:
 	```ts
 	// To run this example:
@@ -22,7 +25,7 @@
 		await using promise2 = use(download('https://example.com/page2.html'));
 
 		// Handle the first task
-		console.log(await promise1);
+		console.log(await promise1); // If this throws, the promise2 will be awaited at the end of the block
 
 		// Handle the second task
 		console.log(await promise2);
@@ -39,7 +42,9 @@ export function use<T>(promise: Promise<T>)
 }
 
 class UsedPromise<T>
-{	#cought = false;
+{	/**	Did the user catch error from this promise?
+	 **/
+	#cought = false;
 
 	constructor(private promise: Promise<T>)
 	{
@@ -49,17 +54,17 @@ class UsedPromise<T>
 		@param onfulfilled The callback to execute when the Promise is resolved.
 		@param onrejected The callback to execute when the Promise is rejected.
 		@returns A Promise for the completion of which ever callback is executed.
-    **/
+	 **/
 	then<Y=T, N=never>(onfulfilled?: ((value: T) => Y|PromiseLike<Y>) | null | undefined, onrejected?: ((reason: unknown) => N|PromiseLike<N>) | null | undefined): Promise<Y|N>
-	{	this.#cought = !!onrejected;
+	{	this.#cought ||= !!onrejected;
 		return this.promise.then(onfulfilled, onrejected);
 	}
 
-    /**	Attaches a callback for only the rejection of the Promise.
-    	@param onrejected The callback to execute when the Promise is rejected.
-    	@returns A Promise for the completion of the callback.
-    **/
-    catch<N=never>(onrejected?: ((reason: unknown) => N|PromiseLike<N>) | null | undefined): Promise<T|N>
+	/**	Attaches a callback for only the rejection of the Promise.
+		@param onrejected The callback to execute when the Promise is rejected.
+		@returns A Promise for the completion of the callback.
+	 **/
+	catch<N=never>(onrejected?: ((reason: unknown) => N|PromiseLike<N>) | null | undefined): Promise<T|N>
 	{	this.#cought = true;
 		return this.promise.catch(onrejected);
 	}
@@ -68,14 +73,17 @@ class UsedPromise<T>
 		resolved value cannot be modified from the callback.
 		@param onfinally The callback to execute when the Promise is settled (fulfilled or rejected).
 		@returns A Promise for the completion of the callback.
-    **/
-    finally(onfinally?: VoidFunction|null|undefined): Promise<T>
+	 **/
+	finally(onfinally?: VoidFunction|null|undefined): Promise<T>
 	{	return this.promise.finally(onfinally);
 	}
 
 	async [Symbol.asyncDispose]()
 	{	if (!this.#cought)
 		{	await this.promise;
+		}
+		else
+		{	await this.promise.catch(() => {});
 		}
 	}
 }
